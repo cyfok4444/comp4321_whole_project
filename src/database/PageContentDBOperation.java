6d3a305aa4a08695cd3ff233b33dfb04c638a5a5
mavishung <tmmhung@connect.ust.hk>
2019-04-25 22:47:32 +0800

    .

--- src/database/PageContentDBOperation.java
+++ src/database/PageContentDBOperation.java
@@ -25,11 +25,31 @@ public class PageContentDBOperation extends DataBaseOperation{
     @Override
     public HashMap getHashMapTable() throws RocksDBException{
         HashMap<Integer,ArrayList<String>>  hashMap = new HashMap<>();
+        RocksIterator iterator = rocksDB.newIterator();
 
+        for(iterator.seekToFirst(); iterator.isValid(); iterator.next()) {
+
+            String key = new String(iterator.key());
+            String value = new String(rocksDB.get(key.getBytes()));
+            String[]s2 = value.split("JOHNMAVISOSCAR");
+            ArrayList<String> arrayList = new ArrayList<>();
+            for ( int i = 0 ; i < s2.length ; i++ ){
+                    arrayList.add(s2[i]);
+                }
+                hashMap.put(Integer.parseInt(key),arrayList);
+
+        }
         return hashMap;
     }
 
-    public void updateDB (String key, String info) throws RocksDBException {
-        rocksDB.merge(key.getBytes(), info.getBytes());
+
+    public void updateDB (String key, String info,HashMap<Integer,ArrayList<String>> hashMap) throws RocksDBException {
+        rocksDB.remove(key.getBytes());
+        for (String in : hashMap.get(Integer.parseInt(key))) {
+            rocksDB.put(key.getBytes(),info.getBytes());
+            rocksDB.put(key.getBytes(),"JOHNMAVISOSCAR".getBytes());
+        }
     }
+
+
 }
